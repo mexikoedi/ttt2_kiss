e250843b93e4df618758c3b5366099078d3a8db5
mexikoedi <mexikoedi@outlook.com>
2024-01-26 19:56:02 +0100

    moved settings (server addons -> equipment menu)

--- gamemodes/terrortown/entities/weapons/weapon_ttt2_kiss/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt2_kiss/shared.lua
@@ -309,6 +309,64 @@ function SWEP:SetupDataTables()
 end
 
 if CLIENT then
+    function SWEP:AddToSettingsMenu(parent)
+        local form = vgui.CreateTTT2Form(parent, "header_equipment_additional")
+        form:MakeCheckBox({
+            serverConvar = "ttt2_kiss_prepare_sound",
+            label = "label_kiss_prepare_sound"
+        })
+
+        form:MakeCheckBox({
+            serverConvar = "ttt2_kiss_primary_sound",
+            label = "label_kiss_primary_sound"
+        })
+
+        form:MakeCheckBox({
+            serverConvar = "ttt2_kiss_secondary_sound",
+            label = "label_kiss_secondary_sound"
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_kiss_damage",
+            label = "label_kiss_damage",
+            min = -100,
+            max = 100,
+            decimal = 0
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_kiss_ammo",
+            label = "label_kiss_ammo",
+            min = 0,
+            max = 100,
+            decimal = 0
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_kiss_clipSize",
+            label = "label_kiss_clipSize",
+            min = 0,
+            max = 100,
+            decimal = 0
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_kiss_length",
+            label = "label_kiss_length",
+            min = 0,
+            max = 5,
+            decimal = 1
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_kiss_delay",
+            label = "label_kiss_delay",
+            min = 0,
+            max = 5,
+            decimal = 1
+        })
+    end
+
     SWEP._kissMult = 0
     SWEP.KissMult = 0
     local npc_attachment_lookup = {
